4665aa86f5d23e238ef247f51a97c95ab7d6d631
srinu <linux@gmail.com>
2024-08-23 04:08:34 +0000

    it is from clone

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args ) {
         System.out.println("Hello, World");
-	System.out.println("Welcome GIT!!!!")
+	System.out.println("Welcome GIT!!!! from clone")
     }
 }
